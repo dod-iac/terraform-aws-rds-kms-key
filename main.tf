9017d4f8cd7da4af5b1045d9888a3d748568b32e
Chris Gilmer <chris.gilmer@gmail.com>
2021-08-24 11:05:29 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,95 @@
+/**
+ * ## Usage
+ *
+ * Creates a KMS key used to encrypt data-at-rest stored in CloudWatch Logs
+ *
+ * ```hcl
+ * module "rds_kms_key" {
+ *   source = "dod-iac/rds-kms-key/aws"
+ *
+ *   name = "alias/name"
+ *
+ *   tags = {
+ *     Application = var.application
+ *     Environment = var.environment
+ *     Automation  = "Terraform"
+ *   }
+ * }
+ * ```
+  *
+ * ## Testing
+ *
+ * Run all terratest tests using the `terratest` script.  If using `aws-vault`, you could use `aws-vault exec $AWS_PROFILE -- terratest`.  The `AWS_DEFAULT_REGION` environment variable is required by the tests.  Use `TT_SKIP_DESTROY=1` to not destroy the infrastructure created during the tests.  Use `TT_VERBOSE=1` to log all tests as they are run.  The go test command can be executed directly, too.
+ *
+ * ## Terraform Version
+ *
+ * Terraform 1.0. Pin module version to ~> 1.0.0 . Submit pull-requests to master branch.
+ *
+ * Previous versions not supported or use at your own risk.
+ *
+ * ## License
+ *
+ * This project constitutes a work of the United States Government and is not subject to domestic copyright protection under 17 USC § 105.  However, because the project utilizes code licensed from contributors and other third parties, it therefore is licensed under the MIT License.  See LICENSE file for more information.
+ */
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "rds" {
+  policy_id = "key-policy-rds"
+  statement {
+    sid = "Enable IAM User Permissions"
+    actions = [
+      "kms:*",
+    ]
+    effect = "Allow"
+    principals {
+      type = "AWS"
+      identifiers = [
+        format(
+          "arn:%s:iam::%s:root",
+          data.aws_partition.current.partition,
+          data.aws_caller_identity.current.account_id
+        )
+      ]
+    }
+    resources = ["*"]
+  }
+  statement {
+    sid = "AllowCloudWatchLogs"
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+    effect = "Allow"
+    principals {
+      type = "Service"
+      identifiers = [
+        format(
+          "rds.%s.amazonaws.com",
+          data.aws_region.current.name
+        )
+      ]
+    }
+    resources = ["*"]
+  }
+}
+
+resource "aws_kms_key" "rds" {
+  description             = var.description
+  deletion_window_in_days = var.key_deletion_window_in_days
+  enable_key_rotation     = "true"
+  policy                  = data.aws_iam_policy_document.rds.json
+  tags                    = var.tags
+}
+
+resource "aws_kms_alias" "rds" {
+  name          = var.name
+  target_key_id = aws_kms_key.rds.key_id
+}
